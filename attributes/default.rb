f0c1361367e2196f6c3fc6f225d5b3e9805e9ca9
Zo Obradovic <ping@obradovic.com>
2012-08-28 14:38:00 -0700

    attribute for git

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,4 @@ default[:graphene][:apache][:port]          = 80
 default[:graphene][:apache][:document_root] = "/opt/graphite/webapp/graphene"
 default[:graphene][:graphite_root]          = "graphite"
 default[:graphene][:git_repo]               = "git://github.com/jondot/graphene.git"
+default[:graphene][:git_executable]         = "git"
